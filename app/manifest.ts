b68a801937475ebfe01a9a41ce4065d30b3f0b3d
Micah Traya <micahtraya16@gmail.com>
2026-09-14 18:20:44 +0800

    Fix HIMIG PWA app icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,9 +12,14 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait-primary",
     icons: [
       {
-        src: "/icon.svg",
-        sizes: "any",
-        type: "image/svg+xml",
+        src: "/himig-icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/himig-icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   };
